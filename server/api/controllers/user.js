0a8771916756660b7a4872ea33ad8ea5ebf1a81e
Anirudh Agrawal <anirudhagrawal2212@gmail.com>
2021-02-21 17:50:06 +0530

    Added controller for registeredCourse

--- server/api/controllers/user.js
+++ server/api/controllers/user.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const User = require('../../models/user');
-const createError = require('http-errors');
+const express = require("express");
+const User = require("../../models/user");
+const createError = require("http-errors");
 
 module.exports = {
-    userInfo: async (req, res) => {
-        if (req.user){
-            res.send(req.user)
-        }
+  userInfo: async (req, res) => {
+    if (req.user) {
+      res.send(req.user);
     }
-}
+  },
+  course: async (req, res) => {
+    let user = req.user;
+    User.findOne({ _id : user.id })
+      .populate("registeredCourses") // only works if we pushed refs to person.eventsAttended
+      .exec(function (err, user) {
+        if (err) return handleError(err);
+        console.log(user.registeredCourses);
+      });
+  },
+};
