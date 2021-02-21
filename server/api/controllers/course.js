6c062be759c9265a0eb2a5b969d26b69b6f3459d
Manu <manugupta1998@gmail.com>
2021-02-21 17:51:34 +0530

    added course enroll api.

--- server/api/controllers/course.js
+++ server/api/controllers/course.js
@@ -5,8 +5,18 @@ const createError = require('http-errors');
 module.exports = {
     list: async (req, res) => {
         Course.find({}).then((courses) => {
-            console.log(courses);
             res.send(courses);
+        }).catch((err) => {
+            res.status(500).send();
+        })
+    },
+    enroll: async (req, res) => {
+        let user = req.user;
+        let courseId = req.params.id;
+        Course.findOne({_id: courseId}).then((course) => {
+            user.registeredCourses.push(course._id);
+            user.save();
+            res.send();
         }).catch((err) => {
             res.status(500).send();
         })
--- server/api/routes/course.js
+++ server/api/routes/course.js
@@ -3,9 +3,9 @@ const courseRouter = express.Router();
 const courseController = require('../controllers/course');
 const authMiddleware = require('../middleware/auth');
 
-courseRouter.get('/course/list', courseController.list);
+courseRouter.get('/course/list', authMiddleware.isAuthenticated, courseController.list);
 
-// courseRouter.get('/course/enroll', authMiddleware.isAuthenticated,userController.userInfo);
+courseRouter.get('/course/enroll', authMiddleware.isAuthenticated,courseController.enroll);
 
 // courseRouter.get('/course', authMiddleware.isAuthenticated,userController.userInfo);
 
