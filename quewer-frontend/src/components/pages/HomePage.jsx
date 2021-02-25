a74c0f635843d699becca45453a7b6a104db2b75
Manu <manugupta1998@gmail.com>
2021-02-26 01:59:30 +0530

    Fixed react env variables.

--- quewer-frontend/src/components/pages/HomePage.jsx
+++ quewer-frontend/src/components/pages/HomePage.jsx
@@ -4,24 +4,26 @@ import '../../Global.css';
 import {quotes, loginPhrase} from '../../GlobalVariables';
 import logo from '../../views/logo.png';
 
-const dotenv = require('dotenv').config();
-
-const loginUrl = process.env.SERVER_URL + "/api/auth/google?uri=" + process.env.CLIENT_URL + "/main"
+var loginUrl = process.env.REACT_APP_SERVER_URL + "/api/auth/google?uri=" + process.env.REACT_APP_CLIENT_URL + "/main"
 
 class HomePage extends React.Component {
-    render() {
-        return (
-            <div className="App FlexCard RowCard">
-            <div className="QuoteCard FlexCard ColumnCard">
-              <h1>{quotes}</h1>
-              <img src={logo} alt="Quewer Logo" id="logoImage"></img>
-            </div>
-            <div className="LoginCard FlexCard ColumnCard">
-              <h3>{loginPhrase}</h3>
-              <button id="loginButton" href={loginUrl}>Login using Google</button>
-            </div>
-          </div>
-        );
-    }
+  login(){
+    window.location.href= loginUrl;
+  }
+  render() {
+    console.log(loginUrl);
+    return (
+      <div className="App FlexCard RowCard">
+      <div className="QuoteCard FlexCard ColumnCard">
+        <h1>{quotes}</h1>
+        <img src={logo} alt="Quewer Logo" id="logoImage"></img>
+      </div>
+      <div className="LoginCard FlexCard ColumnCard">
+        <h3>{loginPhrase}</h3>
+        <button id="loginButton" onClick={this.login}>Login using Google</button>
+      </div>
+      </div>
+    );
+  }
 }
 export default HomePage;
