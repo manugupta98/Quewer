5b900fb3a44c76204bf19df4a5f9ca0c41df3289
Manu <manugupta1998@gmail.com>
2021-03-09 21:09:06 +0530

    Change text family and login phrase.

--- quewer-frontend/src/GlobalVariables.js
+++ quewer-frontend/src/GlobalVariables.js
@@ -1,4 +1,1 @@
-export const quotes = "Afraid of being scoring less ??";
-export const loginPhrase = "ALL KNOWLEDGE IS CONNECTED TO ALL OTHER KNOWLEDGE. THE FUN IS IN MAKING THE CONNECTIONS BY ASKING QUESTIONS.";
-export const client_id = "1099381732350-hgpb11upi2us3u1tld9siij3n20r56bc.apps.googleusercontent.com";
-export const loginError = "Error Authorizing with Google!"
+export const loginPhrase = 'Learn to ask. Ask to Learn.';
