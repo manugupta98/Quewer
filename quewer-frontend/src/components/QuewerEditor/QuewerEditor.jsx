e198e0966c51071063de1a6259738f49f4746401
Parveen Jakhar <1parveenjakhar@gmail.com>
2021-03-21 21:08:05 +0530

    Add Editor

---
+++ quewer-frontend/src/components/QuewerEditor/QuewerEditor.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import './QuewerEditor.css';
+
+class QuewerEditor extends React.Component {
+    state = { editorState: EditorState.createEmpty() }
+    onChange = (editorState) => this.setState({editorState})
+    render() {
+        return (
+            <Editor 
+                editorState={this.state.editorState}
+                wrapperClassName="Wrapper"
+                editorClassName="Editor"
+                onEditorStateChange={this.onChange}
+                toolbar={{
+                    options: ['inline', 'fontSize']
+                }}
+                toolbarClassName="Toolbar"
+            />
+        );
+    }
+}
+
+export default QuewerEditor;
