0d7a7297502050089239b2814c1b95306677967d
Manu <manugupta1998@gmail.com>
2021-04-02 18:21:59 +0530

    unenroll course api.

--- server/services/course.js
+++ server/services/course.js
@@ -10,11 +10,27 @@ class CourseServices{
         await Course.findOne({_id: courseId}).catch((err) => {
             throw createError.NotFound("Course not found", {expose: true});
         }).then((course) => {
-            if (user.registeredCourses.some(item => item == course.id) || course.registeredUsers.some(item => item == user.id)){
+            console.log(course.title);
+            if (user.registeredCourses.some(item => item == course.id) && course.registeredUsers.some(item => item == user.id)){
                 throw createError.Conflict("Already Registered", {expose: true});
             }
             user.registeredCourses.push(course.id,);
             course.registeredUsers.push(user.id,);
+            user.save();
+            course.save();
+        });
+    }
+
+    static async unenroll(user, courseId){
+        await Course.findOne({_id: courseId}).catch((err) => {
+            throw createError.NotFound("Course not found", {expose: true});
+        }).then((course) => {
+            console.log(course.title);
+            if (!(user.registeredCourses.some(item => item == course.id) && course.registeredUsers.some(item => item == user.id))){
+                throw createError.Conflict("Not Registered", {expose: true});
+            }
+            user.registeredCourses.pop(course.id,);
+            course.registeredUsers.pop(user.id,);
             user.save();
             course.save();
         });
