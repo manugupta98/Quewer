0b7aba58342e309c69dc1ac5d2346ef807e3c6ef
ansh1482 <gupta.ansh2000@gmail.com>
2021-04-11 19:14:12 +0530

    add Searchbar

--- quewer-frontend/src/components/DisplayCard/DisplayCard.jsx
+++ quewer-frontend/src/components/DisplayCard/DisplayCard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import Button from '../button';
+import { connect } from 'react-redux';
 import "./DisplayCard.css";
 
-export default function DisplayCard({name}) {
+function DisplayCard({name}) {
   return (
     <div className="DisplayCard">
             <h1>{name}</h1>
@@ -17,10 +18,6 @@ export default function DisplayCard({name}) {
                     <h3>615</h3>
                     <h4>Answers</h4>
                 </div>
-                <div className="QuestionData">
-                    <h3>90K</h3>
-                    <h4>Views</h4>
-                </div>
             </div>
             <div className="ButtonsRow">
                 <Link to="/postquestion"><Button color="#618CFB" textColor="white" text="Ask a question" /></Link>
@@ -29,3 +26,10 @@ export default function DisplayCard({name}) {
         </div>
   );
 }
+
+const mapStateToProps = state => ({
+    numQuestions: state.course.courseList,
+    numAnswers: ''
+});
+
+export default connect(mapStateToProps)(DisplayCard);
