9d37c493735591084566936c48cff73afd21cdb8
Manu <manugupta1998@gmail.com>
2021-04-23 06:01:39 +0530

    admin/user redirect on login.

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const authRouter = express.Router();
 
 authRouter.get('/auth/google',(req, res, next) => {
-  res.cookie('google-login-redirect-url', req.query.uri);
+    res.cookie('google-login-redirect-url', req.query.uri);
   next();
 }, passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.email'] }));
 
@@ -11,10 +11,16 @@ authRouter.get('/auth/google/callback',
   passport.authenticate('google'), (req, res) => {
     var redirectURL = req.cookies['google-login-redirect-url'];
     res.clearCookie('google-login-redirect-url');
-    if (redirectURL){
+    if (redirectURL !== 'undefined'){
       res.redirect(redirectURL);
     }else{
-      res.redirect('/');
+      let url = process.env.CLIENT_URL;
+      if (req.user.type === 'admin'){
+        res.redirect(url + '/admin');
+      }
+      else {
+        res.redirect(url + '/main');
+      }
     }
   });
 
